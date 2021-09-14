69f866049ece7267f2398005490261bffd502d7d
Emmanuel Lopez <jesusemm010@hotmail.com>
2021-09-14 14:10:48 -0500

    Los mensajes se agregan al HTML

--- js/app.js
+++ js/app.js
@@ -19,14 +19,31 @@ function agregarTweet(e) {
     if (tweet === ""){
         mostrarError("No puedes agregar un tweet vacio");
         return;
+    }  
+
+    const tweetOBJ = {
+        id: Date.now(),
+        tweet, //Como es igual el valor de la variable a la variable se asigna directo
     }
+
+    //Añadir al arreglo de tweets
+    tweets = [...tweets, tweetOBJ];
+    console.log(tweets);
+
+    //Crear HTML
+    crearHTML();
+
+    //Reiniciar el formulario
+    formulario.reset();
 }
 function mostrarError(mensaje){
     
+    //Eliminar que se muestre más de una alerta
     const errorPrevio = document.querySelector(".error");
     if(errorPrevio){
         return;
     }
+    //Agregar validacion
     const mensajeError = document.createElement("P");
 
     mensajeError.textContent = mensaje;
@@ -34,11 +51,32 @@ function mostrarError(mensaje){
 
     //insrtarlo en el HTML
     const contenido = document.querySelector("#contenido");
-       
     contenido.appendChild(mensajeError);
 
     //Elimina la alerta despeus de 3 seg
     setTimeout( ()=>{
         mensajeError.remove();
     }, 3000)
+}
+
+function crearHTML() {
+    limpiarHTML();
+    if(tweets.length > 0){
+    }
+    tweets.forEach( tweet => {
+        //Crear el html 
+        const li = document.createElement("li");
+
+        li.textContent = tweet.tweet;
+
+        listaTweets.appendChild(li);
+    });
+}
+
+//limpiar el HTML 
+
+function limpiarHTML () {
+    while(listaTweets.firstChild) {
+        listaTweets.removeChild(listaTweets.firstChild);
+    }
 }
